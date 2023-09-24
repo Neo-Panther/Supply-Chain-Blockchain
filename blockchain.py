ab0e694d4b99cb00fc09b99e085ee8e993bfde01
anmol042298 <138283252+anmol042298@users.noreply.github.com>
2023-09-24 13:30:43 +0530

    indentation

--- blockchain.py
+++ blockchain.py
@@ -3,31 +3,31 @@ import hashlib
 from typing import Dict, List
 
 class Node():
-  def __init__(self, address: str, stake: int, id: str):
+  def __init__(self, address: str, stake: int, id : str):
     self.address = address
     self.stake = stake
     self.id = id
 
 class Transaction():
-  def __init__(self, manufacturer_id: int, product_id: int, client_id: int, distributor_id: int, amount: int):
-    self.manufacturer_id = manufacturer_id
-    self.product_id = product_id
-    self.client_id = client_id
-    self.distributor_id = distributor_id
-    self.amount = amount
-    self.timestamp = datetime.now()
-    self.transaction_id = distributor_id^product_id^client_id
+     def __init__(self, manufacturer_id: int, product_id: int, client_id: int, distributor_id: int, amount: int):
+        self.manufacturer_id = manufacturer_id
+        self.product_id = product_id
+        self.client_id = client_id
+        self.distributor_id = distributor_id
+        self.amount = amount
+        self.timestamp = datetime.now()
+        self.transaction_id = distributor_id^product_id^client_id
   
   def __str__(self) -> str:
     return str(self.__dict__())
   
 class Block():
   def __init__(self, prev_hash: str, height: int, transactions: List[Transaction]):
-    self.previous_hash = prev_hash
-    self.merkle_root = Blockchain.computeMerkleRoot(transactions)
-    self.height = height
-    self.timestamp = datetime.now()
-    self.header_hash = Blockchain.calculateHash(prev_hash + self.merkle_root + height + self.timestamp.strftime("%d|%m|%Y><%H:%M:%S"))
+       self.previous_hash = prev_hash
+       self.merkle_root = Blockchain.computeMerkleRoot(transactions)
+       self.height = height
+       self.timestamp = datetime.now()
+       self.header_hash = Blockchain.calculateHash(prev_hash + self.merkle_root + height + self.timestamp.strftime("%d|%m|%Y><%H:%M:%S"))
 
 class Blockchain():
   def __init__(self):
