1d5e05c9e2cb6c2aeb7934f482d7f9b44e0150ff
anmol042298 <138283252+anmol042298@users.noreply.github.com>
2023-09-28 00:57:57 +0530

    main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from blockchain import *
-
+from random import randint
 print("Creating Blockchain")
 print("Enter initial products with manufacturer-")
 stock = map(int, input("Enter space separated product-ids: ").split())
@@ -15,8 +15,43 @@ while(True):
   print("Get Product Status: 4 \n")
   print("Display blockchain: 5 \n")
   print("mine block : 6 \n")
-
+  address:int=1000  
   s=input("chooose an operation to perform ")
-  
+ 
   if s==1:
-    print()
+   a:str=input('enter input')
+   stock = map(int, input("Enter space separated product-ids: ").split())
+   bc.addNode(address,address*2,a,stock)
+   address-=1
+ 
+  elif s==2:
+    a:int=input('enter node')
+    bc.changeParentNode(a)
+
+  elif s==3:
+    if bc.parent_node.type=='distributor':
+        a:int =input("enter name of client")
+        b:int =input("enter product id")
+        bc.startTransaction(a,b)
+
+    if bc.parent_node.type=='manufacturer':
+        a:int =input("enter name of distributor")
+        b:int =input("enter product id") 
+        bc.startTransaction(a,b)
+
+  elif s==4:
+     print(bc.pending_transactions)
+
+  elif s==5:
+     bc.mineBlock()
+
+  elif s==6:
+     bc.parent_node.getInfo()      
+     
+       
+
+
+
+
+
+    
