bef398913987ced52e1b1cf6681ecc64b48082e9
Aryan Gupta <f20210162@hyderabad.bits-pilani.ac.in>
2023-09-27 22:14:52 +0530

    added some main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from blockchain import *
 
-# # TODO: add the following to api endpoints (to Blockchain.)
-# def addNode(port_address: int, security_deposit: int, type: ):
-
-# # the main function of our code
-# def startVirtualMachine(m_address: int, m_stake: int, m_stock: Iterable[int]) -> None:
-#   manufacturer = Node(m_stake, m_address, m_stock, NodeType.MANUFACTURER)
-#   current_active_nodes[manufacturer.id] = manufacturer
-#   blockchain = Blockchain(manufacturer)
+print("Creating Blockchain")
+print("Enter initial products with manufacturer-")
+stock = map(int, input("Enter space separated product-ids: ").split())
+manufacturer = Node(100000000, 99999, stock, NodeType.MANUFACTURER)
+bc = Blockchain(manufacturer)
+print("Blockchain created")
+while(True){
+  print("Choose an operation to perform:")
+  
+}
